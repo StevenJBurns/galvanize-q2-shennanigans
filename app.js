5ed6a5359a22f463ec2b85e95831359fb261d5bd
SJB <StevenJBurns@users.noreply.github.com>
2017-10-10 14:00:29 -0600

    Non-functional routes

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 var app = express();
+
+let index = require('./routes/index');
+let artists = require('./routes/artists');
+let albums = require('./routes/albums');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,23 +19,24 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/artist', artists);
+app.use('albums', albums);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/artists.js
+++ routes/artists.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: "Express" });
+  res.render('artists', { title: "Express" });
+});
+
+router.get('/:id', (req, res, next) => {
+  res.render('artists', { title: "Express" });
 });
 
 router.post('/'), (req, res, next) => {
